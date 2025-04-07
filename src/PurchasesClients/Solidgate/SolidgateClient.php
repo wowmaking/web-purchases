567be240a446728dceeda885a77f737d71cbfd66
Martishevich Oleg <o.martishevich@wowmaking.net>
2025-04-07 11:35:45 +0300

    AS-9320 solidgate one time products

--- src/PurchasesClients/Solidgate/SolidgateClient.php
+++ src/PurchasesClients/Solidgate/SolidgateClient.php
@@ -342,7 +342,9 @@ class SolidgateClient extends PurchasesClient
     }
 
     public function oneTimePaymentAlternativePayment(string $orderId, int $amount, string $currency, string $productCode, string $token,
-                                                     string $orderDescription, string $email, string $ipAddress, string $deviceId, bool $isIdfm = true,
+                                                     string $orderDescription, string $email, string $ipAddress, string $deviceId,
+                                                     ?int $productId,
+                                                     bool $isIdfm = true,
                                                      array $params = [], $paymentMethod = 'paypal-vault')
     {
         $orderMetadata = [
@@ -379,6 +381,10 @@ class SolidgateClient extends PurchasesClient
 
         if($params){
             $data = array_merge($data, $params);
+        }
+
+        if ($productId !== null) {
+            $data['product_id'] = $productId;
         }
 
         return json_decode(
@@ -579,6 +585,7 @@ class SolidgateClient extends PurchasesClient
         string $orderId,
         string $orderDescription,
         string $deviceId,
+        ?int $productId,
         string $productCode,
         string $customerEmail,
         string $ipAddress,
@@ -588,7 +595,6 @@ class SolidgateClient extends PurchasesClient
         string $signature,
         string $version,
         bool   $isIdfm = true,
-        // сюда тоже прокидываю везде productId
         array $params = []
     )
     {
@@ -629,6 +635,10 @@ class SolidgateClient extends PurchasesClient
             'order_metadata' => $orderMetadata,
         ];
 
+        if ($productId !== null) {
+            $data['product_id'] = $productId;
+        }
+
         if($params){
             $data = array_merge($data, $params);
         }
@@ -645,6 +655,7 @@ class SolidgateClient extends PurchasesClient
         string $orderId,
         string $orderDescription,
         string $deviceId,
+        ?int $productId,
         string $productCode,
         string $customerEmail,
         string $ipAddress,
@@ -691,6 +702,10 @@ class SolidgateClient extends PurchasesClient
             'protocolVersion' => $protocolVersion,
             'order_metadata' => $orderMetadata,
         ];
+
+        if ($productId !== null) {
+            $data['product_id'] = $productId;
+        }
 
         if($params){
             $data = array_merge($data, $params);
@@ -798,25 +813,25 @@ class SolidgateClient extends PurchasesClient
             true);
     }
 
-    public function initAlternativeOneTimePayment(string $paymentMethod, string $orderId, string $productId, int $amount, string $currency,
+    public function initAlternativeOneTimePayment(string $paymentMethod, string $orderId, ?int $productId, string $productCode, int $amount, string $currency,
                                                   string $orderDescription, string $email, string $customerAccountId, string $ipAddress, string $deviceId, bool $isIdfm = true,
                                                   array $params = [])
     {
         $orderMetadata = [
             'idfm' => $deviceId,
-            'one_time_product_code' => $productId
+            'one_time_product_code' => $productCode
         ];
 
         if (!$isIdfm) {
             $orderMetadata = [
                 'idfv' => $deviceId,
-                'one_time_product_code' => $productId
+                'one_time_product_code' => $productCode
             ];
         }
 
         if ($params['solid_metadata'] ?? []) {
             $solidMetadata = $params['solid_metadata'];
-            $solidMetadata['one_time_product_code'] = $productId;
+            $solidMetadata['one_time_product_code'] = $productCode;
             $orderMetadata = $solidMetadata;
         }
 
@@ -832,6 +847,10 @@ class SolidgateClient extends PurchasesClient
             'platform' => 'WEB',
             'order_metadata' => $orderMetadata,
         ];
+
+        if ($productId !== null) {
+            $data['product_id'] = $productId;
+        }
 
         if(isset($params['mercadopagoParams']) && isset($params['mercadopagoParams']['country'])) {
             $data['billing_address'] = ['country'=> $params['mercadopagoParams']['country']];
